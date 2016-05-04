e84da83dc63e58807a51c3788f7357840f1de372
D.Eusebio <dan13use@gmail.com>
2016-05-04 02:16:47 +0200

    Create adapters for trailers and reviews

---
+++ app/src/main/java/com/example/android/popularmovies/TrailersAdapter.java
@@ -0,0 +1,47 @@
+package com.example.android.popularmovies;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.android.popularmovies.model.Trailer;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by debeyo on 02/05/2016.
+ */
+public class TrailersAdapter extends ArrayAdapter<Trailer>{
+
+    static class ViewHolder {
+        @BindView(R.id.list_item_trailer_textview)
+        TextView trailerTextView;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    public TrailersAdapter(final Activity context, final List<Trailer> trailers) {
+        super(context,0 , trailers);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+
+        final Trailer trailer = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_trailer, parent, false);
+        }
+        ViewHolder viewHolder = new ViewHolder(convertView);
+        viewHolder.trailerTextView.setText(trailer.getName());
+        return  convertView;
+    }
+}
